07df4fe2dc313516283c130a7135799066833948
Özgün DOĞAN <ozgundogan2000@gmail.com>
2023-06-08 16:26:51 +0300

    Raw Recipe Application
    Initial commit

---
+++ .NetFiles/RecipeApp/RecipeApp/Models/BufferedSingleFileUploadDb.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeApp.Models
+{
+    public class BufferedSingleFileUploadDb
+    {
+        [Required]
+        [Display(Name = "Photo")]
+        public IFormFile? FormFile { get; set; }
+    }
+}
---
+++ .NetFiles/RecipeApp/RecipeApp/Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RecipeApp.Models;
+
+public partial class Ingredient
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public double? Quantity { get; set; }
+
+    public double? Unit { get; set; }
+
+    public int? RecipeId { get; set; }
+}
